e9184e9df787d55e16cfa3d4a758d4d99e5d10bb
Vladimir Chervanev <vladimir.chervanev@traveltripper.com>
2019-12-07 20:09:43 -0600

    splash screen and intro sound

---
+++ Assets/SplashController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("StartGame", 2f);
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
