e81f343b81ef89e97d46c158429f3408dbcbf5e6
Vladimir Chervanev <vladimir.chervanev@traveltripper.com>
2019-12-14 21:08:38 -0600

    target explosions

--- Assets/Target.cs
+++ Assets/Target.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    [SerializeField] GameObject ExplosionFX;
     private void OnParticleCollision(GameObject other)
     {
+        Instantiate(ExplosionFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
